2d67e8fd8a57e9f7c7bbc2d4a39e51da8eaff01d
Mariano Arturo Ochoa Poveda <mariano@zonadigital.cu>
2024-05-17 22:29:59 -0400

    Actualizando request

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 Route::get('/', function () {
     return view('welcome');
